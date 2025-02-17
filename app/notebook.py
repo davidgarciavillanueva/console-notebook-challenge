122c22e5626bbf82045ff5a1f4f62dae49309b1c
davidddd <davidgarciavillanueva15@gmail.com>
2025-02-17 12:53:30 -0500

    primeroooooo: cracion de la clase

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,16 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+
+    def __init__(self,code:str,title: str, text :str,importance :str,creation_date :datetime ):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = creation_date
