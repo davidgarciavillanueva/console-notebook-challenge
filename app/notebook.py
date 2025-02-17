7bef1f17a0a1a578594104f6d975c92a4e2b6ea0
davidddd <davidgarciavillanueva15@gmail.com>
2025-02-17 13:29:48 -0500

    la otra calsee

--- app/notebook.py
+++ app/notebook.py
@@ -8,9 +8,25 @@ class Note:
     LOW: str = "LOW"
 
 
-    def __init__(self,code:str,title: str, text :str,importance :str,creation_date :datetime ):
+    def __init__(self,code:str,title: str, text :str,importance :str,creation_date :datetime,tags :list[str] ):
         self.code = code
         self.title = title
         self.text = text
         self.importance = importance
-        self.creation_date = creation_date
+        self.tags :list[str] = tags
+
+    def add_tag(self,tag:str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+        else:
+            print(f"ya exite la etiqueta {tag}")
+
+
+    def __str__(self):
+        Date: {creation_date}
+        {title}: {text: }
+        pass
+
+class Notebook:
+    def __init__(self,notes :list[Note] ):
+        self.notes :list[Note] = []
